16d5b65b7cd5fba204bf7d1b6f861aaf23e02e93
Laís Fernandes Daniel <laisfd@hotmail.com>
2022-12-07 17:16:29 -0300

    Add new components on header

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,16 +4,19 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule
   ],
   exports: [
     MatToolbarModule,
     MatExpansionModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatMenuModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
