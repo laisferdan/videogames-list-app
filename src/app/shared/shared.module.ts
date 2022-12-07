ec2ce75688fb4a83cb64354a48d0a7e8fbcc139c
Laís Fernandes Daniel <laisfd@hotmail.com>
2022-12-07 17:18:19 -0300

    Add pipe and directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from './../material/material.module';
 
 import { HeaderComponent } from './header/header.component';
 
+import { ChangeBackgroundDirective } from './directives/change-background.directive';
+import { VideogameDirective } from './directives/videogame.directive';
+import { AlreadyFinishedPipe } from './pipes/already-finished.pipe';
+
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    AlreadyFinishedPipe,
+    VideogameDirective,
+    ChangeBackgroundDirective
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule, 
+    RouterModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    AlreadyFinishedPipe,
+    VideogameDirective,
+    ChangeBackgroundDirective
   ]
 })
 export class SharedModule { }
