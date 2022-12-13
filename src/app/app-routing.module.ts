6cc2094457c54b9349286935e5d1da36881479a3
Laís Fernandes Daniel <laisfd@hotmail.com>
2022-12-13 20:35:35 -0300

    Add create component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { VideogameListComponent } from './pages/videogame-list/videogame-list.component';
+import { VideogameCreateComponent } from './pages/videogame-create/videogame-create.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: "list", component: VideogameListComponent },
+  { path: "create", component: VideogameCreateComponent },
   { path: "", redirectTo: "list", pathMatch: "full" },
   { path: "**", component: PageNotFoundComponent },
 ];
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,22 +1,28 @@
-import { MaterialModule } from './../material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from './../material/material.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { VideogameListComponent } from './videogame-list/videogame-list.component';
 import { VideogameItemComponent } from './videogame-list/videogame-item/videogame-item.component';
+import { VideogameCreateComponent } from './videogame-create/videogame-create.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     VideogameListComponent,
     VideogameItemComponent,
+    VideogameCreateComponent,
     PageNotFoundComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
-    SharedModule
+    SharedModule, 
+    FormsModule, 
+    ReactiveFormsModule
   ],
   exports: [
     VideogameListComponent,
