67987b59311f3c91035c66d06dc57e471b405b8e
Laís Fernandes Daniel <laisfd@hotmail.com>
2022-12-14 19:14:08 -0300

    Add import of dialog and form components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,32 +1,42 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from './../material/material.module';
 
-import { HeaderComponent } from './header/header.component';
-
 import { ChangeBackgroundDirective } from './directives/change-background.directive';
 import { VideogameDirective } from './directives/videogame.directive';
+
 import { AlreadyFinishedPipe } from './pipes/already-finished.pipe';
+
+import { HeaderComponent } from './header/header.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { VideogameFormComponent } from './videogame-form/videogame-form.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     AlreadyFinishedPipe,
     VideogameDirective,
-    ChangeBackgroundDirective
+    ChangeBackgroundDirective,
+    DialogComponent,
+    VideogameFormComponent
   ],
   imports: [
     CommonModule,
     MaterialModule, 
-    RouterModule
+    RouterModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   exports: [
     HeaderComponent,
     AlreadyFinishedPipe,
     VideogameDirective,
-    ChangeBackgroundDirective
+    ChangeBackgroundDirective,
+    DialogComponent, 
+    VideogameFormComponent
   ]
 })
 export class SharedModule { }
